cefc6ae205f8f4a0d3912be77f0c79bd9cdd96ad
Lisa <lavellanalis@gmail.com>
2020-11-14 02:22:25 +0200

    Scarping

--- parsers/async_parser.py
+++ parsers/async_parser.py
@@ -22,10 +22,23 @@ import aiofiles
 import asyncio
 from utils import read_links_txt, get_file_name_from_url
 
-links = read_links_txt('input/images_links.txt')
+from bs4 import BeautifulSoup
+import re
+
+links = 'https://en.wikipedia.org/wiki/Microcontroller'
 img_dir = 'img/async'
 
 pathlib.Path(img_dir).mkdir(parents=True, exist_ok=True)
+
+
+async def parse_links(links):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(links, ssl=False) as resp:
+            bs = BeautifulSoup(await resp.text(), 'html.parser')
+            url = bs.find_all('img', {'src': re.compile('.jpg')})
+            for image in url:
+                yield image['src']
+                # print(image['src'] + '\n')
 
 
 async def download(url):
@@ -35,6 +48,15 @@ async def download(url):
             await f.write(await response.read())
             await f.close()
 
+
+async def main():
+    async for url in parse_links(links):
+        await download(url)
+
+    # result = [download(link) for link in url]
+    # return result
+
 loop = asyncio.get_event_loop()
-tasks = [download(link) for link in links]
-loop.run_until_complete(asyncio.gather(*tasks))
+tasks = parse_links(links)
+# loop.run_until_complete(asyncio.gather(*tasks))
+loop.run_until_complete(main())
